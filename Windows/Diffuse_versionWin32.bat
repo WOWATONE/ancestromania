1f8557d5bb7a26d6a5e130b08fb0d054d88f0059
girouxmatthieu <girouxmatthieu@localhost>
2014-10-04 18:23:28 +0200

    diffuse version file

---
+++ Windows/Diffuse_versionWin32.bat
@@ -0,0 +1,12 @@
+rem diffuse inc extended files
+
+cd %CD%
+
+cd ..
+
+copy ..\i386-win32\Ancestromania.ini  ..\i386-linux\Ancestromania.ini 
+copy ..\i386-win32\Ancestromania.ini  ..\i386-linux-k\Ancestromania.ini 
+copy ..\i386-win32\Ancestromania.ini  ..\x86_64-linux\Ancestromania.ini 
+copy ..\i386-win32\Ancestromania.ini  ..\x86_64-linux-k\Ancestromania.ini 
+copy ..\i386-win32\Ancestromania.ini  ..\x86_64-win64\Ancestromania.ini 
+
