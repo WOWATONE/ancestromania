12a50cbaa34e0b1065956cd4252e5781d2ae7a8c
girouxmatthieu <girouxmatthieu@localhost>
2014-10-31 14:44:44 +0100

    better script

--- Unix/Unsigned/creedebrpm386.sh
+++ Unix/Unsigned/creedebrpm386.sh
@@ -4,16 +4,16 @@
 directory=`dirname $0`
 echo "Working in $directory"
 cd $directory 
-./version.sh ./Update_AncestroIntel/DEBIAN/control ./i386-linux/Ancestromania.ini
-cp i386-linux/Ancestromania Update_AncestroIntel/usr/share/ancestromania
-cp i386-linux/Ancestromania.ini Update_AncestroIntel/usr/share/ancestromania
-cp i386-linux/Ancestromania Update_AncestroIntel.tar
-cp i386-linux/Ancestromania.ini Update_AncestroIntel.tar
-cp i386-linux/Scripts/* Update_AncestroIntel/usr/share/ancestromania/Scripts
-cp i386-linux/Scripts/* Update_AncestroIntel.tar/Scripts
+./version.sh ./Update_AncestroIntel/DEBIAN/control ./../../i386-linux/Ancestromania.ini
+cp ../../i386-linux/Ancestromania Update_AncestroIntel/usr/share/ancestromania
+cp ../../i386-linux/Ancestromania.ini Update_AncestroIntel/usr/share/ancestromania
+cp ../../i386-linux/Ancestromania Update_AncestroIntel.tar
+cp ../../i386-linux/Ancestromania.ini Update_AncestroIntel.tar
+cp ../../i386-linux/Scripts/* Update_AncestroIntel/usr/share/ancestromania/Scripts
+cp ../../i386-linux/Scripts/* Update_AncestroIntel.tar/Scripts
 sudo  chmod 644 Update_AncestroIntel/usr/share/ancestromania/Scripts/*
 sudo  chmod 644 Update_AncestroIntel.tar/Scripts/*
-sudo  chmod 644 i386-linux/Scripts/*
+sudo  chmod 644 ../../i386-linux/Scripts/*
 sudo chown -R root.root Update_AncestroIntel
 sudo dpkg-deb -b Update_AncestroIntel 
 sudo chown -R matthieu Update_AncestroIntel
@@ -22,7 +22,5 @@ rm *el.rpm
 sudo alien -r --scripts Update_AncestroIntel.deb
 sudo chown matthieu *.rpm
 mv *i386*.rpm Update_AncestroIntel.rpm
-sudo chown -R root.root Update_AncestroIntel.tar
-sudo tar -czvf Update_AncestroIntel.tar.gz Update_AncestroIntel.tar
-sudo chown -R matthieu Update_AncestroIntel.tar
+tar -czvf Update_AncestroIntel.tar.gz Update_AncestroIntel.tar
 
