83063165de4fd5eccdbfb8132d31ba02d1015dc6
girouxmatthieu <girouxmatthieu@localhost>
2015-01-01 07:55:33 +0100

    For Windows not tested

---
+++ Windows/install/copyto64.bat
@@ -0,0 +1,4 @@
+copy ..\..\i386-win32\AncestroWeb ..\..\x86_64-win64\
+copy ..\..\i386-win32\Images ..\..\x86_64-win64\
+copy ..\..\i386-win32\Scripts ..\..\x86_64-win64\
+copy ..\..\i386-win32\Ancestromania.ini ..\..\x86_64-win64\
