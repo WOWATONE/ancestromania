754c7bbfee805467f92ad57ca1931e27bd17a8d9
girouxmatthieu <girouxmatthieu@localhost>
2015-01-01 15:58:06 +0100

    testing 64 install

--- Windows/install/copyto64.bat
+++ Windows/install/copyto64.bat
@@ -1,4 +1,5 @@
-copy ..\..\i386-win32\AncestroWeb ..\..\x86_64-win64\
-copy ..\..\i386-win32\Images ..\..\x86_64-win64\
-copy ..\..\i386-win32\Scripts ..\..\x86_64-win64\
-copy ..\..\i386-win32\Ancestromania.ini ..\..\x86_64-win64\
+xcopy ..\..\i386-win32\AncestroWeb ..\..\x86_64-win64\AncestroWeb /S /I
+xcopy ..\..\i386-win32\Images ..\..\x86_64-win64\Images /S /I
+xcopy ..\..\i386-win32\Scripts ..\..\x86_64-win64\Scripts /S /I
+xcopy ..\..\i386-win32\Ancestromania.ini ..\..\x86_64-win64\
+ /I
