470a837f09645ac2fcfdd0a8e3457d9f0587883f
girouxmatthieu <girouxmatthieu@localhost>
2015-01-02 16:24:37 +0100

    testing more

--- Unix/Install/linkupdate.sh
+++ Unix/Install/linkupdate.sh
@@ -6,12 +6,12 @@ cd ..
 
 ln -s ../Windows/Output/Update_AncestroIntel.exe ./Inis/
 ln -s ../Windows/Output/Update_AncestroIntel64.exe ./Inis/
-ln -s ../Update_AncestroIntel.deb ./Inis/
-ln -s ../Update_AncestroIntel.rpm ./Inis/
-ln -s ../Update_AncestroIntel.tar.gz ./Inis/
-ln -s ../Update_AncestroIntel64.deb ./Inis/
-ln -s ../Update_AncestroIntel64.rpm ./Inis/
-ln -s ../Update_AncestroIntel64.tar.gz ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel.deb ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel.rpm ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel.tar.gz ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel64.deb ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel64.rpm ./Inis/
+ln -s ../Unix/Unsigned/Update_AncestroIntel64.tar.gz ./Inis/
 ln -s ../Docs/Evolutions.html ./Inis/
 ln -s ../Docs/Evolutions.rtf ./Inis/
 
